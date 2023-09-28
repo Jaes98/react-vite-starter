dec9362fe3a1a39922f87b5e003ffbb58732b401
Marcus Jaes <marcus.jaes@gmail.com>
2023-09-28 12:08:29 +0200

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import reactLogo from "./assets/react.svg";
+import Member from "./components/member.jsx";
 
 function App() {
     return (
-        <>
-            <header>
-                <a href="https://react.dev" target="_blank" rel="noreferrer">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-                <h1>RACE Your React 🎉</h1>
-            </header>
-        </>
+      <>
+        <header>
+          <a href="https://react.dev" target="_blank" rel="noreferrer">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+          <h1>RACE Your React 🎉</h1>
+        </header>
+        <Member member={{ club: "REAL MADRID SIUUU" }}></Member>
+      </>
     );
 }
+
 
 export default App;
---
+++ src/components/Member.jsx
@@ -0,0 +1,3 @@
+export default function Member({member}) {
+  return (<p>{member.Club}</p>);
+}
